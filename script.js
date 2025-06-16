83490ea9208e4835a9ec0e95b115587dd938726a
DANERICOM <146855706+DANERICOM@users.noreply.github.com>
2025-06-16 09:00:23 -0300

    código para diminuir e aumentar tamanho da senha

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');//puxar o documento do html
 let tamanhoSenha = 12;
+const tamanhoMaxSenha = 20;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-console.log(botoes);
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha -= tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha < tamanhoMaxSenha){
+        tamanhoSenha += 1;
+    }
+    numeroSenha
+}

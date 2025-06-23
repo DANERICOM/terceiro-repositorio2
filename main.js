047e9f872c2456931b5c328105cc9787241ad02e
DANERICOM <146855706+DANERICOM@users.noreply.github.com>
2025-06-23 08:41:07 -0300

    gerar letras maiusculas aleatórias dependendo do tamanho da senha

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-/*
+
 const numeroSenha = document.querySelector('.parametro-senha__texto');//puxar o documento do html
 let tamanhoSenha = 12;
 const tamanhoMaxSenha = 20;
+const tamanhoMinSenha = 8;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -10,10 +11,11 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+    if(tamanhoSenha > tamanhoMinSenha){
         tamanhoSenha -= 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
@@ -21,26 +23,18 @@ function aumentaTamanho(){
         tamanhoSenha += 1;
     }
     numeroSenha.textContent = tamanhoSenha;
-}*/
-const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;
-
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
-
-function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
-        tamanhoSenha = tamanhoSenha - 1;
-    }
-    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-function aumentaTamanho() {
-    if (tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha + 1;
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function geraSenha(){
+    let senha = '';
+    for(let i=0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.irandom()*letrasMaiusculas.length;
+        //numeroAleatorio = Math.floor(numeroAleatorio);
     }
-    numeroSenha.textContent = tamanhoSenha;
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+    campoSenha.value = letrasMaiusculas;
 }

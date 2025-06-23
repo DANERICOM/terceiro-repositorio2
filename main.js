cec6c89531ae1fa33ae591a9bba6fd1deb3c236c
DANERICOM <146855706+DANERICOM@users.noreply.github.com>
2025-06-23 07:57:11 -0300

    teste3

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const numeroSenha = document.querySelector('.parametro-senha__texto');//puxar o documento do html
 let tamanhoSenha = 12;
 const tamanhoMaxSenha = 20;
@@ -18,6 +19,28 @@ function diminuiTamanho(){
 function aumentaTamanho(){
     if(tamanhoSenha < tamanhoMaxSenha){
         tamanhoSenha += 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}*/
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
 }

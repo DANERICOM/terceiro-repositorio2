ddb0413dcaca49b73a50a4683b0b73cc91bc15ce
DANERICOM <146855706+DANERICOM@users.noreply.github.com>
2025-07-07 08:57:24 -0300

    adicionando entropia

--- main.js
+++ main.js
@@ -20,41 +20,41 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 //aumentar e diminuir a quantidade de caracteres na senha
-    function diminuiTamanho(){
-        if(tamanhoSenha > tamanhoMinSenha){
-            tamanhoSenha -= 1;
-        }
-        numeroSenha.textContent = tamanhoSenha;
-        geraSenha();
+function diminuiTamanho(){
+    if(tamanhoSenha > tamanhoMinSenha){
+        tamanhoSenha -= 1;
     }
-    function aumentaTamanho(){
-        if(tamanhoSenha < tamanhoMaxSenha){
-            tamanhoSenha += 1;
-        }
-        numeroSenha.textContent = tamanhoSenha;
-        geraSenha();
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < tamanhoMaxSenha){
+        tamanhoSenha += 1;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
 geraSenha();
 function geraSenha(){
     //só gerar determinados caracteres caso a checkbox deles estejam positivos
     let alfabeto = '';
-        //maiusculas 
-        if(checkbox[0].checked){
-            alfabeto = alfabeto + letrasMaiusculas;
-        }
-        //minusculas
-        if(checkbox[1].checked){
-            alfabeto = alfabeto + letrasMinusculas;
-        }
-        //numeros
-        if(checkbox[2].checked){
-            alfabeto = alfabeto + numeros;
-        }
-        //simbolos
-        if(checkbox[3].checked){
-            alfabeto = alfabeto + simbolos;
-        }
-
+    //maiusculas 
+    if(checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    //minusculas
+    if(checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    //numeros
+    if(checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    //simbolos
+    if(checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    
     //realmente gerar a senha
     let senha = '';
     for(let i=0; i < tamanhoSenha; i++){
@@ -63,19 +63,23 @@ function geraSenha(){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
 
 //FORÇA DA SENHA
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
+    let entropia=tamanhoSenha*Math.log2(alfabeto.length);//variação para ajudar a definir a força da senha
+    console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if(tamanhoSenha>=12){
+    if(entropia>=57){
         forcaSenha.classList.add('forte');
-    }else if(tamanhoSenha<12){
-        if(tamanhoSenha > 8){
+    }else if(entropia<57){
+        if(tamanhoSenha > 35){
             forcaSenha.classList.add('media');
-        }else if(tamanhoSenha<=8){
+        }else if(tamanhoSenha<=35){
             forcaSenha.classList.add('fraca');
         }
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2*Math.floor(entropia)/(100e6*60*60*24);
 }
